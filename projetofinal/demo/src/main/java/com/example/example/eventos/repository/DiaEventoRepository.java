dcc7c176df390f436da827a9373aefc55adb490a
Nilton Barbosa Santos Filho <71855454+nbsfilho@users.noreply.github.com>
2024-03-05 16:29:25 -0300

    Delete projetofinal/demo directory

--- projetofinal/demo/src/main/java/com/example/example/eventos/repository/DiaEventoRepository.java
+++
@@ -1,6 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DiaEventoRepository extends JpaRepository<DiaEvento, Long> {
-}
